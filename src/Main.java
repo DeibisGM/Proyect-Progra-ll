5800c45ba8259ae89c4639397c2fd761931b3c99
DeibisGM <john.gutierrez.morales@est.una.ac.cr>
2023-05-30 13:32:32 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("pollo Worlllsld!");
+        System.out.println("pierna");
 
         //hola
         
