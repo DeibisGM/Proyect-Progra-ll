8e8ce2680473123696116f43047a9c9585e9f9de
DeibisGM <jhongutierrezmorales517@gmail.com>
2023-05-30 13:48:49 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-
-        System.out.println("pierna");
-
-        System.out.println("pollo Worlllsld!");
-        System.out.println("pollo Worlllsld!");
-        System.out.println("pollo Worlllsld!");
-
-        //hola
-        
-
     }
 
 }
